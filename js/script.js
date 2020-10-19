93e0ddb994cc119e5a799b7071060f012a0a2069
CrisKLP <klepadlo.krzysztof@gmail.com>
2020-10-19 17:41:04 +0200

    Fixed aria label

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const firstName = document.querySelector('[data-first-name]');
-const lastName = document.querySelector('[data-last-name]');
+const firstName = document.querySelector('[data-firstName]');
+const lastName = document.querySelector('[data-lastName]');
 const email = document.querySelector('[data-email]');
 const password = document.querySelector('[data-password]');
 const submitBtn = document.querySelector('.form__submit-btn');
@@ -32,6 +32,8 @@ const checkForm = e => {
 };
 
 submitBtn.addEventListener('click', checkForm);
-formFields.forEach(field => field.addEventListener('keyup', () => {
-    field.parentElement.classList.remove('error')
-}));
+formFields.forEach(field =>
+    field.addEventListener('keyup', () => {
+        field.parentElement.classList.remove('error');
+    })
+);
